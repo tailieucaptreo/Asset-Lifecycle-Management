3ac34660df068a21d14073332ae06be508a2e429
tailieucaptreo <levantrong011091@gmail.com>
2026-04-13 12:59:07 +0700

    fix backend structure chuẩn deploy

--- backend/routes/device.routes.js
+++ backend/routes/device.routes.js
@@ -1,1 +1,12 @@
-router.post("/import", auth, isAdmin, upload.single("file"), importExcel);
+const express = require("express");
+const router = express.Router();
+
+const {
+  getDevices,
+  importExcel
+} = require("../controllers/device.controller");
+
+router.get("/", getDevices);
+router.post("/import", importExcel);
+
+module.exports = router;
