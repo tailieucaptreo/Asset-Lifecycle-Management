d7e1da667d61f0578ad32f27d08f522a63e64cec
Le Trong <levantrong011091@gmail.com>
2026-04-24 15:27:57 +0700

    Update device.routes.js

--- backend/routes/device.routes.js
+++ backend/routes/device.routes.js
@@ -7,7 +7,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/", controller.getDevices);
 router.post("/", controller.createDevice);
-router.put("/:id", controller.updateDevice); // ✅ phải tồn tại
+router.put("/:id", controller.updateDevice);   // 👈 BẮT BUỘC PHẢI CÓ
 router.delete("/:id", controller.deleteDevice);
 router.post("/import", upload.single("file"), controller.importExcel);
 
