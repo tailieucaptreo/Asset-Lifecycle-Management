67fd7668e9e52be4c221062d432e990bd61b563a
Le Trong <levantrong011091@gmail.com>
2026-04-26 08:47:51 +0700

    Update DeviceList.jsx

--- frontend/src/pages/DeviceList.jsx
+++ frontend/src/pages/DeviceList.jsx
@@ -1,26 +1,36 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import API from "../config";
+import { useLocation } from "react-router-dom";
 import Table from "../components/Table";
 
 export default function DeviceList() {
 
   const [data, setData] = useState([]);
+  const location = useLocation();
 
-  const fetchData = async () => {
-    const res = await axios.get(`${API}/api/devices`);
-    setData(res.data);
-  };
+  const params = new URLSearchParams(location.search);
+  const status = params.get("status");
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    axios.get(`${API}/api/devices`)
+      .then(res => {
+
+        let result = res.data;
+
+        if (status) {
+          result = result.filter(d => d.status === status);
+        }
+
+        setData(result);
+      });
+  }, [status]);
 
   return (
     <div className="p-6">
 
       <h1 className="text-xl font-bold mb-4">
-        📋 Tổng thiết bị
+        📋 Danh sách thiết bị {status && `- ${status}`}
       </h1>
 
       <Table data={data} />
