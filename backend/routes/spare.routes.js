ffda0e5d46dccbd8bf2e26262c2dcfc8d0cb6db2
Le Trong <levantrong011091@gmail.com>
2026-05-10 15:14:15 +0700

    Update spare.routes.js

--- backend/routes/spare.routes.js
+++ backend/routes/spare.routes.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 
 const controller = require("../controllers/spare.controller");
 
+const upload = require("../middleware/upload");
+
+// ================= CRUD =================
 router.get("/", controller.getAll);
 
 router.get("/:id", controller.getOne);
@@ -11,5 +14,12 @@ router.post("/", controller.create);
 router.put("/:id", controller.update);
 
 router.delete("/:id", controller.remove);
+
+// ================= UPLOAD IMAGE =================
+router.post(
+  "/upload",
+  upload.single("image"),
+  controller.uploadImage
+);
 
 module.exports = router;
