cacbdb8537c664bf62a3a94982a7070b1b426c57
Le Trong <levantrong011091@gmail.com>
2026-05-16 20:08:50 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+} from "react-router-dom";
+
 import Sidebar from "./components/Sidebar";
+
 import Dashboard from "./pages/Dashboard";
 import AddDevice from "./pages/AddDevice";
 import DeviceList from "./pages/DeviceList";
@@ -12,28 +19,97 @@ import DeviceDetail from "./pages/DeviceDetail";
 import SpareDevices from "./pages/SpareDevices";
 import Login from "./pages/Login";
 
-export default function App() {
+function Layout() {
+
+  const location = useLocation();
+
+  const isLoginPage =
+    location.pathname === "/";
+
   return (
+
+    <div className="flex">
+
+      {!isLoginPage && <Sidebar />}
+
+      <Routes>
+
+        {/* LOGIN */}
+        <Route
+          path="/"
+          element={<Login />}
+        />
+
+        {/* DASHBOARD */}
+        <Route
+          path="/dashboard"
+          element={<Dashboard />}
+        />
+
+        {/* DEVICES */}
+        <Route
+          path="/devices"
+          element={<DeviceList />}
+        />
+
+        <Route
+          path="/devices/expired"
+          element={<ExpiredDevices />}
+        />
+
+        <Route
+          path="/add"
+          element={<AddDevice />}
+        />
+
+        {/* MAINTENANCE */}
+        <Route
+          path="/maintenance/schedule"
+          element={<MaintenanceSchedule />}
+        />
+
+        <Route
+          path="/maintenance/work"
+          element={<WorkOrders />}
+        />
+
+        <Route
+          path="/maintenance/alerts"
+          element={<Alerts />}
+        />
+
+        {/* IMPORT */}
+        <Route
+          path="/import"
+          element={<ImportExcel />}
+        />
+
+        {/* DETAIL */}
+        <Route
+          path="/devices/:id"
+          element={<DeviceDetail />}
+        />
+
+        {/* SPARE */}
+        <Route
+          path="/spare-devices"
+          element={<SpareDevices />}
+        />
+
+      </Routes>
+
+    </div>
+  );
+}
+
+export default function App() {
+
+  return (
+
     <BrowserRouter>
-      <div className="flex">
 
-        <Sidebar />
+      <Layout />
 
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/devices" element={<DeviceList />} />
-          <Route path="/devices/expired" element={<ExpiredDevices />} />
-          <Route path="/add" element={<AddDevice />} />
-          <Route path="/maintenance/schedule" element={<MaintenanceSchedule />} />
-          <Route path="/maintenance/work" element={<WorkOrders />} />
-          <Route path="/maintenance/alerts" element={<Alerts />} />
-          <Route path="/import" element={<ImportExcel />} />
-          <Route path="/devices/:id" element={<DeviceDetail />} />
-          <Route path="/spare-devices" element={<SpareDevices />} />
-          <Route path="/" element={<Login />} />
-        </Routes>
-
-      </div>
     </BrowserRouter>
   );
 }
