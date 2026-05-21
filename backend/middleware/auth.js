4bc406279293e55965f4591cde265ac8da4e2ebc
Le Trong <levantrong011091@gmail.com>
2026-05-21 17:28:27 +0700

    Create auth.js

---
+++ backend/middleware/auth.js
@@ -0,0 +1,43 @@
+const jwt =
+require("jsonwebtoken");
+
+module.exports =
+(req,res,next)=>{
+
+try{
+
+const token =
+req.headers.authorization
+?.split(" ")[1];
+
+if(!token){
+
+return res
+.status(401)
+json({
+message:"Unauthorized"
+});
+
+}
+
+req.user =
+jwt.verify(
+token,
+process.env.JWT_SECRET
+);
+
+next();
+
+}
+
+catch{
+
+res
+.status(401)
+.json({
+message:"Invalid token"
+});
+
+}
+
+};
