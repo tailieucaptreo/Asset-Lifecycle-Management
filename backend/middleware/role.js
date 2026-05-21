b7bb18bfd3dccbd24a4eeb52294c4a80423eade7
Le Trong <levantrong011091@gmail.com>
2026-05-21 17:28:59 +0700

    Create role.js

---
+++ backend/middleware/role.js
@@ -0,0 +1,31 @@
+module.exports =
+(...roles)=>{
+
+return (
+req,
+res,
+next
+)=>{
+
+if(
+!roles.includes(
+req.user.role
+)
+){
+
+return res
+.status(403)
+.json({
+
+message:
+"Không đủ quyền"
+
+});
+
+}
+
+next();
+
+};
+
+};
