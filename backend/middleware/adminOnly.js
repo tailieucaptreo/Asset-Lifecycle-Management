6adfde1968c798019a70aa1e57c7294f39241a70
Le Trong <levantrong011091@gmail.com>
2026-06-10 15:39:45 +0700

    Create adminOnly.js

---
+++ backend/middleware/adminOnly.js
@@ -0,0 +1,21 @@
+module.exports = (req, res, next) => {
+
+  if (!req.user) {
+
+    return res.status(401).json({
+      message: "Unauthorized"
+    });
+
+  }
+
+  if (req.user.role !== "admin") {
+
+    return res.status(403).json({
+      message: "Chỉ admin được phép thao tác"
+    });
+
+  }
+
+  next();
+
+};
