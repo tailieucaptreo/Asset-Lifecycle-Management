39aa7079f563db05d0d59eebafdde3717adf9e2c
tailieucaptreo <levantrong011091@gmail.com>
2026-06-10 19:47:15 +0700

    1006

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -5,14 +5,8 @@ import API from "../config";
 import Header from "../components/Header.jsx";
 import Card from "../components/Card.jsx";
 import Chart from "../components/Chart.jsx";
-<<<<<<< Updated upstream
 import AdvancedFilter from "../components/AdvancedFilter.jsx";
 import Table from "../components/Table.jsx";
-=======
-import Table from "../components/Table.jsx";
-import AdvancedFilter from "../components/AdvancedFilter.jsx";
-//import ImportExcel from "../components/ImportExcel.jsx";
->>>>>>> Stashed changes
 
 import {
   Cpu,
