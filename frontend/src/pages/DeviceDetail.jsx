a4545767990ad1129f5cf435a2bf452d587a20ff
Le Trong <levantrong011091@gmail.com>
2026-07-06 16:57:23 +0700

    Update DeviceDetail.jsx

--- frontend/src/pages/DeviceDetail.jsx
+++ frontend/src/pages/DeviceDetail.jsx
@@ -433,59 +433,59 @@ export default function DeviceDetail() {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
             
                     <Field
-                        label="Tên thiết bị"
-                        name="name"
-                        value={device.name}
-                        edit={edit}
-                        onChange={handleChange}
+                      label="Tên thiết bị"
+                      name="name"
+                      value={device.name}
+                      edit={edit}
+                      onChange={handleChange}
                     />
-            
+      
                     <Field
-                        label="Mã thiết bị"
-                        name="deviceId"
-                        value={device.deviceId}
-                        edit={edit}
-                        onChange={handleChange}
+                      label="Ký hiệu"
+                      name="code"
+                      value={device.code}
+                      edit={edit}
+                      onChange={handleChange}
                     />
-            
+      
                     <Field
-                        label="Ký hiệu"
-                        name="code"
-                        value={device.code}
-                        edit={edit}
-                        onChange={handleChange}
+                      label="Mã ID"
+                      name="deviceId"
+                      value={device.deviceId}
+                      edit={edit}
+                      onChange={handleChange}
                     />
-            
+      
                     <Field
-                        label="Phân loại"
-                        name="category"
-                        value={device.category}
-                        edit={edit}
-                        onChange={handleChange}
+                      label="Mã vật tư"
+                      name="materialCode"
+                      value={device.materialCode}
+                      edit={edit}
+                      onChange={handleChange}
                     />
-            
+      
                     <Field
-                        label="Tuyến"
-                        name="line"
-                        value={device.line}
-                        edit={edit}
-                        onChange={handleChange}
+                      label="Tuyến"
+                      name="line"
+                      value={device.line}
+                      edit={edit}
+                      onChange={handleChange}
                     />
-            
+      
                     <Field
-                        label="Nhà ga"
-                        name="station"
-                        value={device.station}
-                        edit={edit}
-                        onChange={handleChange}
+                      label="Nhà ga"
+                      name="station"
+                      value={device.station}
+                      edit={edit}
+                      onChange={handleChange}
                     />
-            
+      
                     <Field
-                        label="Khu vực"
-                        name="area"
-                        value={device.area}
-                        edit={handleChange}
-                        onChange={handleChange}
+                      label="Khu vực"
+                      name="area"
+                      value={device.area}
+                      edit={edit}
+                      onChange={handleChange}
                     />
             
                 </div>
