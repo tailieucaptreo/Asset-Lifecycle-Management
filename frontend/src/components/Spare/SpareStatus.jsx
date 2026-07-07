3830d5fd741edeb1c925f468aae539e177336a94
Le Trong <levantrong011091@gmail.com>
2026-07-07 20:32:53 +0700

    Update SpareStatus.jsx

--- frontend/src/components/Spare/SpareStatus.jsx
+++ frontend/src/components/Spare/SpareStatus.jsx
@@ -2,13 +2,10 @@ export default function SpareStatus({ status }) {
 
     const styles = {
 
-        "Tốt":
+        "Thiết bị mới":
             "bg-green-100 text-green-700",
 
-        "Đang sử dụng":
-            "bg-blue-100 text-blue-700",
-
-        "Đang sửa":
+        "Đã sử dụng":
             "bg-yellow-100 text-yellow-700",
 
         "Hỏng":
