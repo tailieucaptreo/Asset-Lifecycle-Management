b27dad5e89cabcb256d3e0ed85a26cce9ec7ed5a
Le Trong <levantrong011091@gmail.com>
2026-07-08 14:49:34 +0700

    Update DriveCard.jsx

--- frontend/src/components/Drive/DriveCard.jsx
+++ frontend/src/components/Drive/DriveCard.jsx
@@ -74,7 +74,7 @@ export default function DriveCard({
                 grid
                 grid-cols-1
                 sm:grid-cols-2
-                xl:grid-cols-6
+                xl:grid-cols-4
                 gap-4
                 mb-6
             "
